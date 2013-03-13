7cc866db3fb422804685e5b6fc7b64328bc3b1a7
HK <sauen@me.com>
2013-03-13 13:49:04 +0100

    MainCalendarPanel now takes username as constructor arg

--- fellesprosjekt/src/gui/MainCalendarPanel.java
+++ fellesprosjekt/src/gui/MainCalendarPanel.java
@@ -6,7 +6,7 @@ import model.EventModel;
 
 public class MainCalendarPanel extends JPanel {
 	
-	public MainCalendarPanel(){
+	public MainCalendarPanel(String username){
 		EventModel model = new EventModel(null, null);
 		EventPanel eventPanel = new EventPanel(model);
 		add(eventPanel);
