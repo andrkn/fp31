fd07c528b785d0d796fd9af9e597553a6981370b
HK <sauen@me.com>
2013-03-13 13:04:23 +0100

    Div fixes for MainCalendarPanel

--- fellesprosjekt/src/gui/MainCalendarPanel.java
+++ fellesprosjekt/src/gui/MainCalendarPanel.java
@@ -1,7 +1,19 @@
 package gui;
 
+import java.awt.Event;
+
 import javax.swing.JPanel;
 
-public class MainCalendarPanel extends JPanel {
+import model.EventModel;
+import model.Person;
 
+public class MainCalendarPanel extends JPanel {
+	
+	public MainCalendarPanel(){
+		EventModel model = new EventModel(null, null);
+		EventPanel eventPanel = new EventPanel(model);
+		add(eventPanel);
+		
+	}
+	
 }
