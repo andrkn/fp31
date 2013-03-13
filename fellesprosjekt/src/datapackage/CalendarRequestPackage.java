3d8483d5785df302570de87f179e386fc6428c28
HK <sauen@me.com>
2013-03-13 14:14:12 +0100

    Changed CalReqPsck

--- fellesprosjekt/src/datapackage/CalendarRequestPackage.java
+++ fellesprosjekt/src/datapackage/CalendarRequestPackage.java
@@ -7,7 +7,10 @@ public class CalendarRequestPackage implements DataPackage{
 	private String name;
 	private int group;
 	
-	public CalendarRequestPackage(
+	public CalendarRequestPackage(String name, int group){
+		this.name = name;
+		this.group = group;
+	}
 	
 	public String getName(){
 		return this.name;
