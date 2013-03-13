5122543ac6d7f234de156ab24ad76b139ae64808
HK <sauen@me.com>
2013-03-13 13:35:42 +0100

    Created constructor for MainCalendarPanel

--- fellesprosjekt/src/gui/MainCalendarPanel.java
+++ fellesprosjekt/src/gui/MainCalendarPanel.java
@@ -2,6 +2,13 @@ package gui;
 
 import javax.swing.JPanel;
 
-public class MainCalendarPanel extends JPanel {
+import model.EventModel;
 
+public class MainCalendarPanel extends JPanel {
+	
+	public MainCalendarPanel(){
+		EventModel model = new EventModel(null, null);
+		EventPanel eventPanel = new EventPanel(model);
+		add(eventPanel);
+	}
 }
