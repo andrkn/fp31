bc3db9de1c676c46d88758303c71cef65ba15df4
HK <sauen@me.com>
2013-03-14 15:16:28 +0100

    Changed packages to accomodate new interface

--- fellesprosjekt/src/datapackage/RoomListRequestPackage.java
+++ fellesprosjekt/src/datapackage/RoomListRequestPackage.java
@@ -3,13 +3,27 @@ package datapackage;
 public class RoomListRequestPackage implements DataPackage{
 
 	private int capacity; 
+	private int packageNumber;
+	private int totalPackages;
 	
-	public RoomListRequestPackage(int capacity){
+	public RoomListRequestPackage(int capacity, int packageNumber, int totalPackages){
 		this.capacity = capacity; 
+		this.packageNumber = packageNumber;
+		this.totalPackages = totalPackages;
 	}
 	
 	public int getCapacity(){
 		return capacity;
+	}
+
+	@Override
+	public int getPackageNumber() {
+		return packageNumber;
+	}
+
+	@Override
+	public int getTotalPackages() {
+		return totalPackages;
 	}
 	
 }
