9d12264ba2f7807c0a9d4462a056dcd6c1c432a8
Henrik Bøhler <henrik.boehler@gmail.com>
2013-03-20 13:30:24 +0100

    UpadateSwingWorker fix

--- fellesprosjekt/src/gui/UpdateSwingWorker.java
+++ fellesprosjekt/src/gui/UpdateSwingWorker.java
@@ -16,13 +16,13 @@ import model.Event;
 
 
 public class UpdateSwingWorker extends SwingWorker<Void, Void>{
-	
+
 	private JFrame frame;
 	private JPanel pane;
 	private CalendarPanel panel;
 	private ArrayList<Event> events;
 	private ArrayList<DataPackage> notifications;
-	
+
 	public UpdateSwingWorker(JPanel pane, JFrame frame, CalendarPanel panel){
 		this.frame = frame;
 		this.pane = pane;
@@ -32,29 +32,29 @@ public class UpdateSwingWorker extends SwingWorker<Void, Void>{
 	@Override
 	public Void doInBackground() {
 		while (true) {
-//			System.out.println("it's working!");
+			//			System.out.println("it's working!");
 			events = new ArrayList<Event>();
 			notifications = new ArrayList<DataPackage>();
 			events = ((MainCalendarPanel) pane).requestCalendar(((MainCalendarPanel) pane).getPerson().getUsername()).getEventList();
 			panel.getModel().addEvents(events);
-			
+
 			notifications = ((MainCalendarPanel) pane).requestNotifications();
-			for (DataPackage pack : notifications) {
-				if (((NotificationPackage) pack).getNotificationType() == 0) {
-					//I think this works now, but needs testing. The event name shows up as null... WHY???
-					JOptionPane.showMessageDialog(null, "The event " + ((NotificationPackage) pack).getEventName() + " is cancelled. Press OK to continue.");
-					DataPackage response = ((MainCalendarPanel) pane).sendPackage(new RSVPPackage(1,1,((NotificationPackage)pack).getEventId(),null, ((MainCalendarPanel) pane).getPerson().getUsername()));
-					System.out.println(((ErrorPackage)response).getDescription());
-				}
-			}
 			if (notifications != null) {
+				for (DataPackage pack : notifications) {
+					if (((NotificationPackage) pack).getNotificationType() == 0) {
+						//I think this works now, but needs testing. The event name shows up as null... WHY???
+						JOptionPane.showMessageDialog(null, "The event " + ((NotificationPackage) pack).getEventName() + " is cancelled. Press OK to continue.");
+						DataPackage response = ((MainCalendarPanel) pane).sendPackage(new RSVPPackage(1,1,((NotificationPackage)pack).getEventId(),null, ((MainCalendarPanel) pane).getPerson().getUsername()));
+						System.out.println(((ErrorPackage)response).getDescription());
+					}
+				}
 				for (Event e : events) {
 					for (DataPackage pack : notifications) {
 						if (((NotificationPackage) pack).getEventId() == e.getEventId()) {
 							if (((NotificationPackage) pack).getNotificationType() == 1) {
 								//Do like this to retrive the answer from the user.
 								int answer = JOptionPane.showConfirmDialog(null, "The event " + e.getName() + " was updated. Are you still able to join?", "Your event was updated", JOptionPane.YES_NO_CANCEL_OPTION); 
-								
+
 								if (answer == JOptionPane.YES_OPTION) {
 									DataPackage response = ((MainCalendarPanel) pane).sendPackage(new RSVPPackage(1,1,e.getEventId(),true, ((MainCalendarPanel) pane).getPerson().getUsername()));
 									System.out.println(((ErrorPackage)response).getDescription());
@@ -66,7 +66,7 @@ public class UpdateSwingWorker extends SwingWorker<Void, Void>{
 							}
 							else if (((NotificationPackage) pack).getNotificationType() == 2) {
 								int answer = JOptionPane.showConfirmDialog(null, "You are invited to the event " + e.getName() + ". Do you want to join?", "Your are invited", JOptionPane.YES_NO_CANCEL_OPTION); 
-							
+
 								if(answer == JOptionPane.YES_OPTION) {
 									DataPackage response = ((MainCalendarPanel) pane).sendPackage(new RSVPPackage(1,1,e.getEventId(),true, ((MainCalendarPanel) pane).getPerson().getUsername()));
 									System.out.println(((ErrorPackage)response).getDescription());
@@ -80,10 +80,10 @@ public class UpdateSwingWorker extends SwingWorker<Void, Void>{
 					}
 				}
 			}
-			
+
 			try {
 				//Sleeping for 20 seconds
-				Thread.sleep(20000);
+				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -93,6 +93,6 @@ public class UpdateSwingWorker extends SwingWorker<Void, Void>{
 
 	@Override
 	public void done() {
-		
+
 	}
 }
