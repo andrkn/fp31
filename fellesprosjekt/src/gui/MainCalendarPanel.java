c3ba73cf9ed9a2b28a213abd629ad6e1511faef5
Henrik Bøhler <henrik.boehler@gmail.com>
2013-03-20 15:56:45 +0100

    Sys.out in calendar panel

--- fellesprosjekt/src/gui/MainCalendarPanel.java
+++ fellesprosjekt/src/gui/MainCalendarPanel.java
@@ -139,7 +139,7 @@ public class MainCalendarPanel extends JPanel {
 		sender.sendPackage(new NotificationRequestPackage(1, 1, this.getPerson().getUsername()));
 		response = sender.receivePackageArray();
 		if (response.get(0) instanceof ErrorPackage) {
-			System.out.println("There are no new notifications!");
+			System.out.println(((ErrorPackage)response.get(0)).getDescription());
 			return null;
 		}
 		else {
